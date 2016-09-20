d7f565881242559b714de238cd0f507edeae900b
alberiolima <alberio@dadox.com.br>
2016-09-20 14:13:39 -0300

    Versão que já pode ser usada para testar as portas e vê a saida no monitor serial

--- controle_rc.ino
+++ controle_rc.ino
@@ -1,7 +1,8 @@
 /* 
  *  Controle sem fio usando controle remoto do PS2
  * 
- *  Auto: Albério Lima
+ *  Autor: Albério Lima
+ *  (conforme outras pessoas forem alterando o código seu nome será colocado aqui)
  *  
  */
 
@@ -13,12 +14,51 @@
 #define ctrl_joy2_y      A3 //Segundo joystick movimento horizontal
 #define ctrl_joy2_botao  A1 //Segundo joystick botão
 
+#define intervaloEnvio   50 //Intervalo entre os envios
+
+unsigned long tempoEnviar = 0;
+
 void setup() {
+
+  //Iniciar porta serial para depuração
+  Serial.begin( 9600 );
   
+  
+  //Definição da portas de entrada, que serão usados pelos joysticks
+  pinMode( ctrl_joy1_x, INPUT );
+  pinMode( ctrl_joy1_y, INPUT );
+  pinMode( ctrl_joy1_botao, INPUT );
+  pinMode( ctrl_joy2_y, INPUT );
+  pinMode( ctrl_joy2_x, INPUT );
+  pinMode( ctrl_joy2_botao, INPUT );
+
+  //Calibragem 
 
 }
 
 void loop() {
-  
+
+  if ( millis() >= tempoEnviar ) {
+    tempoEnviar = millis() + intervaloEnvio;
+
+    int joy1_X = analogRead( ctrl_joy1_x );
+    int joy1_Y = analogRead( ctrl_joy1_y );
+    boolean joy1_BOTAO = digitalRead( ctrl_joy1_botao );
+    
+    int joy2_X = analogRead( ctrl_joy2_y );
+    int joy2_Y = analogRead( ctrl_joy2_x );
+    boolean joy2_BOTAO = digitalRead( ctrl_joy2_botao );
+
+    Serial.print( joy1_X );
+    Serial.print( ' ' );
+    Serial.print( joy1_Y );
+//    Serial.print( ' ' );
+    
+//    Serial.print( joy2_X );
+//    Serial.print( ' ' );
+//    Serial.print( joy2_Y );
+    Serial.println();
+    
+  }
 
 }
