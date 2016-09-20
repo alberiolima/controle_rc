0e51ef9cf2c5287502f1b7924190bac4a9721a44
alberiolima <alberio@dadox.com.br>
2016-09-20 13:49:35 -0300

    commit inicial de teste

---
+++ controle_rc.ino
@@ -0,0 +1,24 @@
+/* 
+ *  Controle sem fio usando controle remoto do PS2
+ * 
+ *  Auto: Albério Lima
+ *  
+ */
+
+#define ctrl_joy1_x      A0 //Primeiro joystick movimento vertical
+#define ctrl_joy1_y      A1 //Primeiro joystick movimento horizontal
+#define ctrl_joy1_botao  A1 //Primeiro joystick botão
+
+#define ctrl_joy2_x      A2 //Segundo joystick movimento vertical
+#define ctrl_joy2_y      A3 //Segundo joystick movimento horizontal
+#define ctrl_joy2_botao  A1 //Segundo joystick botão
+
+void setup() {
+  
+
+}
+
+void loop() {
+  
+
+}
