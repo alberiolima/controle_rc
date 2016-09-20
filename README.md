71107866e399c45c979da2823dff03db9a37415a
alberiolima <alberio@dadox.com.br>
2016-09-20 19:43:12 -0300

    versão 0.1 pronta para teste

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # controle_rc
-Controle remoto rc usando controle de PS2
+Construção de um controle remoto sem fio usando um controle de PS2
+
+*** Material necessário
+Controle PS2
+Arduino promini 8mhz 3.3V
+Bateria lipo 3,7V 1200mah
+Modulo wireless (NRF24L01)
+Módulo carregador de bateria TP4056
+
+*** Alcance desejado 100m
--- controle_rc.ino
+++ controle_rc.ino
@@ -5,60 +5,115 @@
  *  (conforme outras pessoas forem alterando o código seu nome será colocado aqui)
  *  
  */
+#include <SPI.h>
+#include <RF24.h>
 
-#define ctrl_joy1_x      A0 //Primeiro joystick movimento vertical
-#define ctrl_joy1_y      A1 //Primeiro joystick movimento horizontal
-#define ctrl_joy1_botao  A1 //Primeiro joystick botão
-
-#define ctrl_joy2_x      A2 //Segundo joystick movimento vertical
-#define ctrl_joy2_y      A3 //Segundo joystick movimento horizontal
-#define ctrl_joy2_botao  A1 //Segundo joystick botão
-
+#define ctrl_joy0_botao   2 //Primeiro joystick botão
+#define ctrl_joy1_botao   3 //Segundo joystick botão
 #define intervaloEnvio   50 //Intervalo entre os envios
+
+struct potenciometro {
+  int maximo=0;
+  int minimo=2048;
+  int meio;
+  int porta;
+  int valor=0;
+};
+potenciometro potenc[4];
 
 unsigned long tempoEnviar = 0;
 
-void setup() {
+const uint64_t pipe = 0xE8E8F0F0E1LL;
 
-  //Iniciar porta serial para depuração
-  Serial.begin( 9600 );
+RF24 radio(9, 10);
+
+void setup() {
   
+  //Iniciar porta serial para depuração
+  Serial.begin( 9600 );  
+  
+  //Definicao das portas de entradas para os potenciometros
+  potenc[0].porta = A0;
+  potenc[1].porta = A1;
+  potenc[2].porta = A2;
+  potenc[3].porta = A3;  
   
   //Definição da portas de entrada, que serão usados pelos joysticks
-  pinMode( ctrl_joy1_x, INPUT );
-  pinMode( ctrl_joy1_y, INPUT );
+  pinMode( ctrl_joy0_botao, INPUT );
   pinMode( ctrl_joy1_botao, INPUT );
-  pinMode( ctrl_joy2_y, INPUT );
-  pinMode( ctrl_joy2_x, INPUT );
-  pinMode( ctrl_joy2_botao, INPUT );
+  for( int pot=0;pot<4;pot++){
+    pinMode( potenc[pot].porta, INPUT );
+  }
 
-  //Calibragem 
+  //Calibragem
+  for(int i = 0;i<50;i++){
+    for(int pot = 0;pot < 4; pot++){
+      potenc[pot].valor += analogRead( potenc[pot].porta );   
+    }
+  }
+  for (int pot = 0;pot < 4; pot++){
+    potenc[pot].meio = (potenc[pot].valor / 50 );
+  }
+  
+  Serial.println( "meios" );
+  for (int pot = 0;pot < 4; pot++){
+    Serial.print( potenc[pot].meio  );
+    Serial.print( ' ' );  
+  }  
+  Serial.println();
+
+  radio.begin();   
+
+  radio.setAutoAck(false);    
+  radio.setDataRate(RF24_250KBPS);    
 
 }
 
 void loop() {
 
+  
+
   if ( millis() >= tempoEnviar ) {
     tempoEnviar = millis() + intervaloEnvio;
 
-    int joy1_X = analogRead( ctrl_joy1_x );
-    int joy1_Y = analogRead( ctrl_joy1_y );
-    boolean joy1_BOTAO = digitalRead( ctrl_joy1_botao );
+    //Trada os quatro potenciômetros
+    int dados_enviar = 0;
+    for (int pot = 0;pot < 4; pot++){
+      potenc[pot].valor = analogRead( potenc[pot].porta );    
+      if ( potenc[pot].valor < potenc[pot].minimo ) {
+        potenc[pot].minimo = potenc[pot].valor;
+      } else if (potenc[pot].valor > potenc[pot].maximo ) {
+        potenc[pot].maximo = potenc[pot].valor;
+      }
+    }
     
-    int joy2_X = analogRead( ctrl_joy2_y );
-    int joy2_Y = analogRead( ctrl_joy2_x );
-    boolean joy2_BOTAO = digitalRead( ctrl_joy2_botao );
+    //ler botão do joystick 1
+    boolean joy0_BOTAO = digitalRead( ctrl_joy1_botao );
 
-    Serial.print( joy1_X );
-    Serial.print( ' ' );
-    Serial.print( joy1_Y );
-//    Serial.print( ' ' );
-    
-//    Serial.print( joy2_X );
-//    Serial.print( ' ' );
-//    Serial.print( joy2_Y );
-    Serial.println();
-    
+    //ler botão do joystick 2
+    boolean joy1_BOTAO = digitalRead( ctrl_joy1_botao );
+
+    //Envia os dados via radio
+    radioTX();
+
+    for (int pot = 0;pot < 4; pot++){
+      Serial.print( potenc[pot].valor  );
+      Serial.print( ' ' );  
+    }
+    Serial.println();    
   }
 
 }
+
+void radioTX(){
+  byte data[16];
+  for (int pot = 0;pot < 4; pot++){
+    data[pot+pot] = potenc[pot].valor / 256;
+    data[pot+pot+1] = potenc[pot].valor - ( data[pot+pot] * 256 );  
+  }
+  
+  radio.stopListening();
+  radio.openWritingPipe(pipe);
+  radio.write( &data, sizeof(data) );  
+}
+
