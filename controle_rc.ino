d629255a7c614f540db1f4ae4b05772ea361bc90
alberiolima <alberio@dadox.com.br>
2016-09-23 21:41:09 -0300

    Usando rádio se8r01

--- controle_rc.ino
+++ controle_rc.ino
@@ -4,19 +4,35 @@
  *  Autor: Albério Lima
  *  (conforme outras pessoas forem alterando o código seu nome será colocado aqui)
  *  
+ * 
+ * 
+ * 
+ * pinagem modulo wireless
+ * 
+ * Nome  se8r01   UNO
+ * 3.3V   1       3.3V
+ * GND    2       GND
+ * CE     3       7
+ * CSN    4       10
+ * SCK    5       13
+ * MOSI   6       11
+ * MISO   7       12
+ * IRQ    8       8
  */
-#include <RF24.h>
+
+ 
+#include "se8r01.h"
 
 #define ctrl_joy0_botao   2 //Primeiro joystick botão
 #define ctrl_joy1_botao   3 //Segundo joystick botão
 #define intervaloEnvio   50 //Intervalo entre os envios
 
 struct potenciometro {
-  int maximo=0;
-  int minimo=2048;
+  int maximo = 0;
+  int minimo = 2048;
   int meio;
   int porta;
-  int valor=0;
+  int valor = 0;
 };
 potenciometro potenc[4];
 
@@ -24,7 +40,18 @@ unsigned long tempoEnviar = 0;
 
 const uint64_t pipe = 0xE8E8F0F0E1LL;
 
-RF24 radio(9, 10);
+const int PAYLOAD_WIDTH = 12;
+
+void init_radio() {
+  if (!init_rf(10, 7, 8, PAYLOAD_WIDTH)) {
+    Serial.println("Chip not found!");
+    while (1);
+  }
+  setPower(POWER_5dbm);
+  selectTxPipe(0);
+  setRtr(15);
+  changeMode(TX_MODE);  
+}
 
 void setup() {
   
@@ -51,7 +78,7 @@ void setup() {
     }
   }
   for (int pot = 0;pot < 4; pot++){
-    potenc[pot].meio = (potenc[pot].valor / 50 );
+    potenc[pot].meio = ( potenc[pot].valor / 50 );
   }
   
   Serial.println( "meios" );
@@ -61,16 +88,11 @@ void setup() {
   }  
   Serial.println();
 
-  radio.begin();   
-
-  radio.setAutoAck(false);    
-  radio.setDataRate(RF24_250KBPS);    
+  init_radio();
 
 }
 
-void loop() {
-
-  
+void loop() {  
 
   if ( millis() >= tempoEnviar ) {
     tempoEnviar = millis() + intervaloEnvio;
@@ -105,14 +127,17 @@ void loop() {
 }
 
 void radioTX(){
-  byte data[16];
+  byte data[12];
   for (int pot = 0;pot < 4; pot++){
     data[pot+pot] = potenc[pot].valor / 256;
     data[pot+pot+1] = potenc[pot].valor - ( data[pot+pot] * 256 );  
   }
+  data[8]=0;
+  data[9]=0;
+  data[10]=0;
+  data[11]=0;
   
-  radio.stopListening();
-  radio.openWritingPipe(pipe);
-  radio.write( &data, sizeof(data) );  
+  sendWithAck(data);
+
 }
 
